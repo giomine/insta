12807a7fc236b26839e24c37245e4ec330a2dd3a
giorgia-mineo <giomineo@gmail.com>
2023-03-23 09:54:37 +0000

    Added postSchema with embedded comments schema in models folder

---
+++ models/posts.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true, maxLength: 1000 },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+}, {
+  timestamps: true,
+})
+
+const postSchema = new mongoose.Schema({
+  caption: { type: String, required: true, maxLength: 1000 },
+  image: { type: String, required: true },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  comments: [commentSchema],
+})
+
+// ? virtual fields here? eg: number of likes
+// postSchema.virtual('numOfLikes')
+//   .get(function(){
+//   })
+// 
+// postSchema.set('toJSON', { virtuals: true })
+
+export default mongoose.model('Post', postSchema)
