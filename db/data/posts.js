8183530dd5945561448239cb5d00ddbbf91bd96f
giorgia-mineo <giomineo@gmail.com>
2023-03-24 16:08:03 +0000

    Added more post seeds

--- db/data/posts.js
+++ db/data/posts.js
@@ -21,6 +21,46 @@ export default [
   },
   {
     caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque magni inventore mollitia tempore accusamus commodi deleniti numquam itaque esse sunt, ratione beatae quod, vitae, aperiam consequatur! Rerum quia libero laudantium.',
-    image: 'https://http.cat/205',
+    image: 'https://http.cat/207',
+  },
+  {
+    caption: 'A fluffy white cat sitting on a blue chair',
+    image: 'https://http.cat/101',
+  },
+  {
+    caption: 'Two cats sleeping on a bed',
+    image: 'https://http.cat/102',
+  },
+  {
+    caption: 'A grey cat wearing a collar and looking up',
+    image: 'https://http.cat/100',
+  },
+  {
+    caption: 'A black and white cat sitting on a windowsill',
+    image: 'https://http.cat/103',
+  },
+  {
+    caption: 'A ginger cat playing with a ball of yarn',
+    image: 'https://http.cat/300',
+  },
+  {
+    caption: 'A kitten with big blue eyes sitting on a cushion',
+    image: 'https://http.cat/301',
+  },
+  {
+    caption: 'A tabby cat lying in a sunbeam',
+    image: 'https://http.cat/302',
+  },
+  {
+    caption: 'A white and black cat with a butterfly on his nose',
+    image: 'https://http.cat/303',
+  },
+  {
+    caption: 'A calico cat peeking out from under a blanket',
+    image: 'https://http.cat/304',
+  },
+  {
+    caption: 'A sleepy grey cat curled up on a cushion',
+    image: 'https://http.cat/305',
   }
 ]
