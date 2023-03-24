ed6f72dc2bfe4e344b6d44f140d16702532ec0d3
Matea <matea.nikolac95@gmail.com>
2023-03-24 11:19:02 +0100

    added more objects in data/posts.js

--- db/data/posts.js
+++ db/data/posts.js
@@ -6,5 +6,21 @@ export default [
   {
     caption: 'dog',
     image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fadvice%2Fg1921%2Flarge-dog-breeds%2F&psig=AOvVaw3kVfGnFE4gO8npnBaHqnmM&ust=1679657677811000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKChuMv68f0CFQAAAAAdAAAAABAE',
+  },
+  {
+    caption: 'cat',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCat&psig=AOvVaw1m8RO_ys-SlP8qSFBAWvmw&ust=1679657604708000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPDByKj68f0CFQAAAAAdAAAAABAE',
+  },
+  {
+    caption: 'dog',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fadvice%2Fg1921%2Flarge-dog-breeds%2F&psig=AOvVaw3kVfGnFE4gO8npnBaHqnmM&ust=1679657677811000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKChuMv68f0CFQAAAAAdAAAAABAE',
+  },
+  {
+    caption: 'cat',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FCat&psig=AOvVaw1m8RO_ys-SlP8qSFBAWvmw&ust=1679657604708000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPDByKj68f0CFQAAAAAdAAAAABAE',
+  },
+  {
+    caption: 'dog',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fadvice%2Fg1921%2Flarge-dog-breeds%2F&psig=AOvVaw3kVfGnFE4gO8npnBaHqnmM&ust=1679657677811000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKChuMv68f0CFQAAAAAdAAAAABAE',
   }
 ]
