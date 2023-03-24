8cdc781dabbe90667cc6777a6067ca9ee3153472
James Nellist <j.nellist97@gmail.com>
2023-03-24 15:20:19 +0000

    login

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
 import './styles/main.scss'
 import App from './App.js'
 
