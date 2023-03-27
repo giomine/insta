a7e80681404c25782644954e10d7a61e2e77c457
giorgia-mineo <giomineo@gmail.com>
2023-03-27 12:47:00 +0100

    Profile page gets posts for logged in user only

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,29 +1,72 @@
 import DisplayPosts from './post/DisplayPosts.js'
+import { getToken } from '../helpers/auth.js'
 import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Profile = () => {
 
   const navigate = useNavigate()
 
+  const [ userInfo, setUserInfo ] = useState()
+
   const handleClick = () => {
     navigate('/posts')
   }
 
+  useEffect(() => {
+    const getProfile = async () => {
+      try {
+        const response = await axios.get('/api/profile', {
+          headers: {
+            Authorization: `Bearer ${getToken()}`,
+          },
+        })
+        console.log('response')
+        // console.log(response.data)
+        setUserInfo(response.data)
+        console.log(response.data)
+      } catch (err){
+        console.log(err)
+      }
+    }
+    getProfile()
+  }, [])
+
+
   return (
     <>
-      {/* <h1>Profile Page</h1> */}
-      <div className='homepage'>
-        <div className='profile-top'>
-          <div className='profile-picture'></div>
-          <div className='profile-right'>
-            <div className='username'>Username</div>
-            <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
-            <button onClick={handleClick}>Create new post</button>
+      {userInfo ? 
+        <>
+          <div className='homepage'>
+            <div className='profile-top'>
+              <div className='profile-picture'></div>
+              <div className='profile-right'>
+                <div className='username'>{userInfo.username}</div>
+                <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
+                <button onClick={handleClick}>Create new post</button>
+              </div>
+            </div>
+          
+            {userInfo.posts.length > 0 ?
+              userInfo.posts.map(post => {
+                const { _id, caption, image, owner } = post
+                // console.log(_id, caption, image, owner.username)
+                return (
+                  <div className='post-card' key={_id}>
+                    <div className='username'><h4>{userInfo.username}</h4></div>
+                    <div className='post-image' style={{ backgroundImage: `url('${image}')` }}></div>
+                    <div className='caption'>{caption}</div>
+                  </div>
+                )
+              })
+              : <>Error</> 
+            }
+            {/* <DisplayPosts /> */}
           </div>
-        </div>
-      
-        <DisplayPosts />
-      </div>
+        </>
+        : 'Please log in!'
+      }
     </>
   )
 }
