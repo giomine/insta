9de48705da8de24916009dd5afd2da7b71160134
giorgia-mineo <giomineo@gmail.com>
2023-03-27 11:19:24 +0100

    Basic profile page added

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,30 @@
+import DisplayPosts from './post/DisplayPosts.js'
+import { useNavigate } from 'react-router-dom'
+
 const Profile = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/posts')
+  }
+
   return (
-    <h1>Profile Page</h1>
+    <>
+      {/* <h1>Profile Page</h1> */}
+      <div className='homepage'>
+        <div className='profile-top'>
+          <div className='profile-picture'></div>
+          <div className='profile-right'>
+            <div className='username'>Username</div>
+            <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
+            <button onClick={handleClick}>Create new post</button>
+          </div>
+        </div>
+      
+        <DisplayPosts />
+      </div>
+    </>
   )
 }
 
