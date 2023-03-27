284c40aa24ce0d69b4a02cc31f534659a6128af2
James Nellist <j.nellist97@gmail.com>
2023-03-27 17:17:18 +0100

    singleposts1

--- client/src/components/post/SinglePost.js
+++ client/src/components/post/SinglePost.js
@@ -1,6 +1,77 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { isAuthenticated } from '../../helpers/auth'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 const SinglePost = () => {
+
+  const [post, setPost] = useState(null)
+
+  const { id } = useParams()
+  console.log('Params ---->', id)
+
+  const [formFields, setFormFields] = useState({
+    comment: '',
+  })
+
+  const handleChange = (e) => {
+    setFormFields({ ...formFields, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.get(`/api/posts/${id}`)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const { data } = await axios.get(`/api/posts/${id}`)
+        console.log('data', data)
+        setPost(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getPost()
+  }, [])
+
   return (
-    <h1>Single Post Page</h1>
+    <main className='single-post-page'>
+      <Container>
+        <Row>
+          {post &&
+            <>
+              <Col>
+                <img src={post.image} alt={post.caption} />
+              </Col>
+              <Col>
+                <h2>{post.owner.username}</h2>
+                <h2>{post.caption}</h2>
+                <h2>{post.comments}</h2>
+              </Col>
+            </>
+          }
+        </Row>
+      </Container>
+      <h1>Single Post Page</h1>
+      <Col as="form" onSubmit={handleSubmit}>
+        <label htmlFor="comment">Comment</label>
+        <input type="text" name="comment" placeholder="enter a comment here" onChange={handleChange} value={formFields.comment} />
+        <button>submit</button>
+      </Col>
+
+    </main>
+
   )
 }
 
