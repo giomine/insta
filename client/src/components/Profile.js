1c1a13796ce834d518e788b769ca2026998498d2
giorgia-mineo <giomineo@gmail.com>
2023-03-27 16:05:17 +0100

    Fixed profile styles

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -37,32 +37,37 @@ const Profile = () => {
   return (
     <>
       {userInfo ? 
-        <>
-          <div className='homepage'>
+        <> 
+          <div className='homepage profilepage'>
             <div className='profile-top'>
               <div className='profile-picture'></div>
               <div className='profile-right'>
-                <div className='username'>{userInfo.username}</div>
+                <div className='profile-username'>{userInfo.username}</div>
                 <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
                 <button onClick={handleClick}>Create new post</button>
               </div>
             </div>
           
-            {userInfo.posts.length > 0 ?
-              userInfo.posts.map(post => {
-                const { _id, caption, image, owner } = post
-                // console.log(_id, caption, image, owner.username)
-                return (
-                  <DisplayPosts 
-                    key={_id}
-                    username={userInfo.username}
-                    image={image}
-                    caption={caption}
-                  />
-                )
-              })
-              : <>Error</> 
-            }
+            <div className='photo-center'>
+              <div className='photos-wrapper'>
+                {userInfo.posts.length > 0 ?
+                  userInfo.posts.map(post => {
+                    const { _id, caption, image, owner } = post
+                    // console.log(_id, caption, image, owner.username)
+                    return (
+                      <DisplayPosts 
+                        key={_id}
+                        // username={userInfo.username}
+                        image={image}
+                        // caption={caption}
+                      />
+                    )
+                  })
+                  : <>Error</> 
+                }
+              </div>
+            </div>
+
           </div>
         </>
         : 'Please log in!'
