58307c77efff39296e2fef243b68b2185c70f63c
giorgia-mineo <giomineo@gmail.com>
2023-03-28 09:58:17 +0100

    Changed from flexbox to grid

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -39,38 +39,33 @@ const Profile = () => {
     <>
       {userInfo ? 
         <> 
-          <div className='homepage profilepage'>
-            <div className='profile-top'>
-              <div className='profile-picture'></div>
-              <div className='profile-right'>
-                <div className='profile-username'>{userInfo.username}</div>
-                <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
-                <button onClick={handleClick}>Create new post</button>
-              </div>
+          <div className='profile-top'>
+            <div className='profile-picture'></div>
+            <div className='profile-right'>
+              <div className='profile-username'>{userInfo.username}</div>
+              <div className='bio'>Bio Quisquam obcaecati sint tempora, facilis.</div>
+              <button onClick={handleClick}>Create new post</button>
             </div>
+          </div>
           
-            <div className='photo-center'>
-              <div className='photos-wrapper'>
-                {userInfo.posts.length > 0 ?
-                  userInfo.posts.map(post => {
-                    const { _id, caption, image, owner } = post
-                    // console.log(_id, caption, image, owner.username)
-                    return (
-                      <Link key={_id} to={`/posts/${_id}`}>
-                        <DisplayPosts 
-                          _id={_id}
-                          // username={userInfo.username}
-                          image={image}
-                          // caption={caption}
-                        />
-                      </Link>
-                    )
-                  })
-                  : <>Error</> 
-                }
-              </div>
-            </div>
-
+          <div className='homepage profilepage'>
+            {userInfo.posts.length > 0 ?
+              userInfo.posts.map(post => {
+                const { _id, caption, image, owner } = post
+                // console.log(_id, caption, image, owner.username)
+                return (
+                  <Link key={_id} to={`/posts/${_id}`}>
+                    <DisplayPosts 
+                      _id={_id}
+                      // username={userInfo.username}
+                      image={image}
+                      // caption={caption}
+                    />
+                  </Link>
+                )
+              })
+              : <>Error</> 
+            }
           </div>
         </>
         : 'Please log in!'
