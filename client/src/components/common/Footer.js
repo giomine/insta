db22bc9c4fa24f5bd80aedad5252189fcf08c2c7
giorgia-mineo <giomineo@gmail.com>
2023-03-30 10:07:18 +0100

    Footer content added

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,8 +1,17 @@
 const Footer = () => {
 
   return (
-    <div className="footer">
-      Footer!
+    <div className="footer-container">
+      <div className="footer">
+        <div className="item">Instagram</div>
+        <div className="item">About</div>
+        <div className="item">Blog</div>
+        <div className="item">Jobs</div>
+        <div className="item">Help</div>
+        <div className="item">UK</div>
+        <div className="item">&copy; Insta</div>
+        <div className="item">Matea, James, & Giorgia</div>
+      </div>
     </div>
   )
 }
