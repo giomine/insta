026f0663d1338200f234b53a677369e954c0c639
giorgia-mineo <giomineo@gmail.com>
2023-03-30 18:00:33 +0100

    Updated footer with brand name

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -5,13 +5,12 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer">
-        <Link to='/' className="item">Instagram</Link>
+        <Link to='/' className="item">&copy; Instasham</Link>
         <Link to='/' className="item">About</Link>
         <Link to='/' className="item">Blog</Link>
         <Link to='/' className="item">Jobs</Link>
         <Link to='/' className="item">Help</Link>
         <Link to='/' className="item">UK</Link>
-        <Link to='/' className="item">&copy; Insta</Link>
         <Link to='/' className="item">Matea, James, & Giorgia</Link>
       </div>
     </div>
